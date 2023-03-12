98fc6301e5e3759f75aeb95b1eb48a7c39db82f6
carrizoja@gmail.com <carrizoja@gmail.com>
2023-03-12 16:45:41 -0300

    Carousel finished

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -26,14 +26,23 @@ const Carousel = () => {
         className="mySwiper container__carousel"
       >
         <SwiperSlide >
-          <div className='container__carousel__img carousel__img__mother'>
+          <div className='container__carousel__img carousel__img__abstract'>
+            <h3>"Innovación es lo que distingue a un líder de los demás."</h3>
+            <p>Steve Jobs</p>
           </div>   
         </SwiperSlide>
         <SwiperSlide>
-          <div className='container__carousel__img carousel__img__mother2'>
+          <div className='container__carousel__img carousel__img__abstract2'>
+            <h3>"La verdadera medida del éxito no es si tienes un problema, sino cómo lo solucionas."</h3>
+            <p>Linus Torvalds</p>
           </div>   
         </SwiperSlide>
-       
+        <SwiperSlide>
+          <div className='container__carousel__img carousel__img__abstract3'>
+          <h3>"Si no puedes hacer que funcione, entonces no es un buen negocio."</h3>
+            <p>Bill Gates</p>
+          </div>   
+        </SwiperSlide>   
         
       </Swiper>
     </>
